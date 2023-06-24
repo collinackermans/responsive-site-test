b2b303997add472c4cd64e88f3b13aa834230c2a
Collin Ackermans <90319742+collinackermans@users.noreply.github.com>
2023-06-24 18:04:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,47 +1,5 @@
-let btnNav=$('.mobile-nav-toggle');
-let navPanel = $('nav ul');
-let main = $('main');
-
-
-btnNav.click(function(){
-
-
-    if (navPanel.hasClass('open')) {
-
-        navPanel.removeClass('open');
-        main.removeClass('nav-open');
-    } 
-    
-    else {
-    
-        navPanel.addClass('open');
-        main.addClass('nav-open');
-    }
 
 
 
-});
 
 
-let navOpen = $('.mobile-nav-toggle');
-
-
-
-btnNav.click(function(){
-
-
-    if (navOpen.hasClass('open')) {
-
-        navOpen.removeClass('open');
-        main.removeClass('nav-open');
-    } 
-    
-    else {
-    
-        navOpen.addClass('open');
-        main.addClass('nav-open');
-    }
-
-
-
-});
